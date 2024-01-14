9b1e5c82c171c6dbdebcab94a2a7b2ff4662f90e
Stent <stentonian@protonmail.com>
2024-01-14 21:53:26 +0000

    Duplicate script from g16

--- scripts/batch_ecdsa.sh
+++ scripts/batch_ecdsa.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# Ran this script on an r7a.8xl (256GB RAM & 32 4th generation AMD EPYC processors)
+# after running the machine_initialization script.
+
+# Benchmarks:
+#  1. compiling circuit:
+#    - time: 15m (c++)
+#    - max cpu: 3%
+#    - max mem: 8%
+#    - non-linear constraints: 32_451_349
+#    - linear constraints: 21_55_310
+#  2. generating witness: 3m
+#  3. checking witness: 9m
+#  4. generating zkey 0:
+#    - time: 8h (old script took 27h)
+#    - max cpu: 21%
+#    - max mem: 60%
+#  5. contributing to phase 2 ceremony:
+#    - time: 15m
+#    - max cpu: 93%
+#    - max mem: 1%
+#  6. verifying final zkey:
+#    - time: 8h
+#    - max cpu: 67%
+#    - max mem: 20%
+#  7. generating proof:
+#    - time: 45s (old script took 11.5h)
+#    - max cpu: 48%
+#    - max mem: 17%
+
 ############################################
 ########### ERROR HANDLING #################
 ############################################
@@ -36,10 +65,6 @@ trap 'err_report $LINENO $ERR_MSG' ERR
 
 # Time command with backslash prefix:
 # https://unix.stackexchange.com/questions/497094/time-not-accepting-arguments
-
-# Format for the `time` command (see `man time` for more details).
-export TIME="STATS: time ([H:]M:S) %E ; mem %KKb ; cpu %P"
-
 function execute {
     ERR_MSG="ERROR $MSG"
     printf "\n================ $MSG ================\n"
@@ -48,19 +73,29 @@ function execute {
 }
 
 ############################################
-################# SNARK ####################
+################## SETUP ###################
 ############################################
 
-# Circuit compilation commands were originally from yo-sun/circom-pairing:
-# https://github.com/yi-sun/circom-pairing/blob/3c43adafa4d908c72f79651a62bd9481fe50fde9/scripts/signature/build_signature.sh
-# And this doc:
-# https://hackmd.io/@yisun/BkT0RS87q
+# Circuit compilation commands were originally taken from https://hackmd.io/@yisun/BkT0RS87q
 
 CIRCUITS_DIR=./circuits
+<<<<<<< Updated upstream
 PHASE1=./powersOfTau28_hez_final_22.ptau
+=======
+PHASE1=./powersOfTau28_hez_final_26.ptau
+>>>>>>> Stashed changes
 BUILD_DIR=./build/batch_ecdsa
 CIRCUIT_NAME=batch_ecdsa
 SIGNALS=./scripts/batch_ecdsa_inputs.json
+
+# Format for the `time` command (see `man time` for more details).
+export TIME="STATS: time ([H:]M:S) %E ; mem %KKb ; cpu %P"
+
+# For the non-patched node version. snarkjs requires lots of memory.
+export NODE_OPTIONS="--max-old-space-size=200000"
+
+# For the patched node version.
+NODE_CLI_OPTIONS="--max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc"
 
 if [ -f "$PHASE1" ]; then
     echo "Found Phase 1 ptau file $PHASE1"
@@ -76,6 +111,7 @@ if [ ! -d "$BUILD_DIR" ]; then
     mkdir -p "$BUILD_DIR"
 fi
 
+<<<<<<< Updated upstream
 
 MSG="COMPILING CIRCUIT"
 # add `--sym` to generate circuit.sym (a symbols file required for debugging and printing the constraint system in an annotated mode)
@@ -94,7 +130,117 @@ execute node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$
 # MSG="GENERATING WITNESS"
 # execute ./"$CIRCUIT_NAME" ../../../"$SIGNALS" ../witness.wtns
 # cd -
+=======
+if [[ -z "$PATCHED_NODE_PATH" ]]; then
+    echo "PATCHED_NODE_PATH env var not set. Must be set for optimized phase 2 trusted setup."
+    graceful_exit 1
+fi
 
-execute npx snarkjs wtns check "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$BUILD_DIR"/witness.wtns
+if [[ -z "$SNARKJS_PATH" ]]; then
+    echo "SNARKJS_PATH env var not set. Must be set for optimized trusted setup."
+    graceful_exit 1
+fi
 
+if [[ -z "$RAPIDSNARK_PATH" ]]; then
+    echo "RAPIDSNARK_PATH env var not set. Must be set for optimized proof generation."
+    graceful_exit 1
+fi
 
+############################################
+################ COMMANDS ##################
+############################################
+
+MSG="COMPILING CIRCUIT"
+# --O1 optimization only removes “equals” constraints but does not optimize out “linear” constraints.
+# the further --O2 optimization takes significantly longer on large circuits (for reasons that aren’t totally clear)
+# time: 15m with c++ (50m with wasm)
+# non-linear constraints: 32_451_349
+# linear constraints: 21_55_310
+execute circom "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom --O1 --r1cs --sym --c --output "$BUILD_DIR" -l ./node_modules
+
+MSG="COMPILING C++ WITNESS GENERATION CODE"
+cd "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
+# time: 20s
+execute make
+
+MSG="GENERATING WITNESS"
+# time: 3m
+execute ./"$CIRCUIT_NAME" ../../../"$SIGNALS" ../witness.wtns
+cd -
+>>>>>>> Stashed changes
+
+MSG="CHECKING WITNESS"
+# took 9m
+execute snarkjs wtns check "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$BUILD_DIR"/witness.wtns
+
+MSG="CONVERTING WITNESS TO JSON"
+# took 1.5 hrs then I killed it
+# execute snarkjs wej "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/witness.json
+
+MSG="GENERATING ZKEY 0"
+# time: 8hrs
+execute "$PATCHED_NODE_PATH" $NODE_CLI_OPTIONS "$SNARKJS_PATH" zkey new "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey
+
+MSG="CONTRIBUTING TO PHASE 2 CEREMONY"
+# time: 15m
+execute snarkjs zkey contribute "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey --name="First contributor" -e="random text for entropy"
+
+# Proving key
+MSG="GENERATING FINAL ZKEY"
+# what is this random hex? https://github.com/iden3/snarkjs#20-apply-a-random-beacon
+# execute npx snarkjs zkey beacon "$BUILD_DIR"/"$CIRCUIT_NAME"_1.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
+
+MSG="VERIFYING FINAL ZKEY"
+# time: 8h
+execute npx snarkjs groth16 verify "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
+
+MSG="EXPORTING VKEY"
+# time: <1s
+execute "$PATCHED_NODE_PATH" "$SNARKJS_PATH" zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json
+
+MSG="GENERATING PROOF FOR SAMPLE INPUT"
+# time: <1m
+execute "$RAPIDSNARK_PATH" "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
+
+MSG="VERIFYING PROOF FOR SAMPLE INPUT"
+execute "$PATCHED_NODE_PATH" "$SNARKJS_PATH" groth16 verify "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
+
+############################################
+############# OLD COMMANDS #################
+############################################
+
+MSG="PRINTING CIRCUIT INFO"
+# took 2hrs to use 128GB of ram, then I killed it
+# execute npx snarkjs r1cs info "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs
+
+MSG="GENERATING ZKEY 0"
+# fails after 20 min with
+#
+# terminate called after throwing an instance of 'std::bad_alloc'
+#   what():  std::bad_alloc
+#
+# then I set `sysctl -w vm.max_map_count=655300`
+# https://github.com/iden3/snarkjs/issues/397#issuecomment-1876700914
+# https://github.com/nodejs/node/issues/27715#issuecomment-578557226
+# https://stackoverflow.com/questions/38558989/node-js-heap-out-of-memory/59923848#59923848
+#
+# takes 27 hours & used 167GB mem
+# execute npx snarkjs groth16 setup "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey
+
+MSG="CONTRIBUTING TO PHASE 2 CEREMONY"
+# execute npx snarkjs zkey contribute "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_1.zkey --name="First contributor" -e="random text for entropy"
+
+MSG="VERIFYING FINAL ZKEY"
+# time: 6 hours
+# execute npx snarkjs zkey verify "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey
+
+MSG="EXPORTING VKEY"
+# Quick, ~ 1 min
+# execute npx snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json
+
+MSG="GENERATING PROOF FOR SAMPLE INPUT"
+# Time: 11.5 hrs
+# execute npx snarkjs groth16 prove "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
+
+MSG="VERIFYING PROOF FOR SAMPLE INPUT"
+# execute npx snarkjs groth16 verify "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
