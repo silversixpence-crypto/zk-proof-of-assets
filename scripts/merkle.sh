dc6478053734babbdff261709810c50accaac728
Stent <stentonian@protonmail.com>
2024-02-05 16:56:57 +0000

    Circuit & script for verifying merkle proof
    This will be used as the method of verifying membership of the anon set

---
+++ scripts/merkle.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# for height 24 there are 5856 linear constraints, and no non-linear constraints
+
+############################################
+########### ERROR HANDLING #################
+############################################
+
+# http://stackoverflow.com/questions/35800082/ddg#35800451
+set -eE
+
+# call like `graceful_exit <exit_code>`
+function graceful_exit {
+    printf "\n####### EXITING... #######\n"
+    DEFAULT_EXIT_CODE=0
+    exit ${1:-$DEFAULT_EXIT_CODE} # use first param or default to 0
+}
+
+# Print line number & message of error and exit gracefully
+# call like `err_report $LINENO $ERR_MSG`
+function err_report {
+    ret=$? # previous command exit value
+    printf "\n####### A COMMAND FAILED ON LINE $1 #######\n\n"
+    echo "Error message: ${@:2}"
+    graceful_exit "$ret"
+}
+
+# can change this in the functions below to get nicer error messages
+# make sure to set back to "UNKNOWN" at end of function scope
+ERR_MSG="UNKNOWN"
+
+# global error catcher
+trap 'err_report $LINENO $ERR_MSG' ERR
+
+############################################
+################# SNARK ####################
+############################################
+
+# Circuit compilation commands were originally from 0xPARC/circom-ecdsa
+# Time command with backslash prefix: https://unix.stackexchange.com/questions/497094/time-not-accepting-arguments
+
+CIRCUITS_DIR=./circuits
+PHASE1=./powersOfTau28_hez_final_18.ptau
+BUILD_DIR=./build/merkle
+CIRCUIT_NAME=merkle
+SIGNALS=./scripts/merkle_inputs.json
+
+# Format for the `time` command (see `man time` for more details).
+export TIME="STATS: time ([H:]M:S) %E ; mem %KKb ; cpu %P"
+
+# snarkjs requires lots of memory.
+# export NODE_OPTIONS="--max-old-space-size=200000"
+
+if [ -f "$PHASE1" ]; then
+    echo "Found Phase 1 ptau file $PHASE1"
+    # TODO check file hash matches https://github.com/iden3/snarkjs#7-prepare-phase-2
+    # TODO verify ptau file https://github.com/iden3/snarkjs#8-verify-the-final-ptau
+else
+    echo "Phase 1 ptau file not found: $PHASE1"
+    graceful_exit 1
+fi
+
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "No build directory found. Creating build directory..."
+    mkdir -p "$BUILD_DIR"
+fi
+
+WORDS="COMPILING CIRCUIT"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+# TODO we don't need --c & --wasm (we should select 1 that we will use to generate the witness)
+# TODO what is --wat?
+# sym: generates circuit.sym (a symbols file required for debugging and printing the constraint system in an annotated mode).
+\time --quiet circom "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom --r1cs --wasm --sym --c --wat --output "$BUILD_DIR" -l ./node_modules
+
+WORDS="GENERATING WITNESS FOR SAMPLE INPUT"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME"_js/"$CIRCUIT_NAME".wasm "$SIGNALS" "$BUILD_DIR"/witness.wtns
+
+WORDS="GENERATING ZKEY 0"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs groth16 setup "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey
+
+WORDS="CONTRIBUTING TO PHASE 2 CEREMONY"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs zkey contribute "$BUILD_DIR"/"$CIRCUIT_NAME"_0.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_1.zkey --name="First contributor" -e="random text for entropy"
+
+# Proving key
+WORDS="GENERATING FINAL ZKEY"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+# what is this random hex? https://github.com/iden3/snarkjs#20-apply-a-random-beacon
+\time --quiet npx snarkjs zkey beacon "$BUILD_DIR"/"$CIRCUIT_NAME"_1.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey 12FE2EC467BD428DD0E966A6287DE2AF8DE09C2C5C0AD902B2C666B0895ABB75 10 -n="Final Beacon phase2"
+
+WORDS="VERIFYING FINAL ZKEY"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs zkey verify "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey
+
+WORDS="EXPORTING VKEY"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs zkey export verificationkey "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json -v
+
+WORDS="GENERATING PROOF FOR SAMPLE INPUT"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs groth16 prove "$BUILD_DIR"/"$CIRCUIT_NAME"_final.zkey "$BUILD_DIR"/witness.wtns "$BUILD_DIR"/proof.json "$BUILD_DIR"/public.json
+
+WORDS="VERIFYING PROOF FOR SAMPLE INPUT"
+ERR_MSG="ERROR $WORDS"
+printf "\n================ $WORDS ================\n"
+\time --quiet npx snarkjs groth16 verify "$BUILD_DIR"/"$CIRCUIT_NAME"_vkey.json "$BUILD_DIR"/public.json "$BUILD_DIR"/proof.json
