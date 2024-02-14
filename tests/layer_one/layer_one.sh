ba7a85f41e9908c0a86a87e4a0a93f77b731483e
Stent <stentonian@protonmail.com>
2024-02-14 13:11:40 +0000

    Add layer one input instead of using input from experiments dir

--- tests/layer_one/layer_one.sh
+++ tests/layer_one/layer_one.sh
@@ -8,7 +8,7 @@ LAYER_ONE_DIRECTORY="$(dirname "$LAYER_ONE_PATH")"
 CIRCUIT_PATH="$LAYER_ONE_DIRECTORY"/layer_one_2_sigs.circom
 BUILD_DIR="$LAYER_ONE_DIRECTORY"/../../build/tests/layer_one
 PTAU_PATH="$LAYER_ONE_DIRECTORY"/../../powersOfTau28_hez_final_22.ptau
-SIGNALS="$LAYER_ONE_DIRECTORY"/../../experiments/scripts/batch_ecdsa_input_prep/input_2.json
+SIGNALS="$LAYER_ONE_DIRECTORY"/input.json
 
 "$LAYER_ONE_DIRECTORY"/../../scripts/g16_setup.sh -B "$BUILD_DIR" "$CIRCUIT_PATH" "$PTAU_PATH"
 "$LAYER_ONE_DIRECTORY"/../../scripts/g16_prove.sh -B "$BUILD_DIR" "$CIRCUIT_PATH" "$SIGNALS"
