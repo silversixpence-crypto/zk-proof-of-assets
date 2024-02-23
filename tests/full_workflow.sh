f6bf94c370f7d892b203ca5731272d08649e0329
Stent <stentonian@protonmail.com>
2024-02-23 11:31:15 +0000

    Update full_workflow.sh to have nice logging

--- tests/full_workflow.sh
+++ tests/full_workflow.sh
@@ -13,15 +13,31 @@ POA_INPUT="$FULL_WORKFLOW_DIRECTORY/input_data_for_2_wallets.json"
 MERKLE_ROOT="$TESTS"/merkle_root.json
 MERKLE_PROOFS="$TESTS"/merkle_proofs.json
 
-npx ts-node "$TESTS"/generate_test_input.ts --num-sigs 2 --message "message to sign"
-npx ts-node "$TESTS"/generate_anon_set.ts --num-addresses 100
-npx ts-node "$SCRIPTS"/merkle_tree.ts  --anonymity-set "$TESTS"/anonymity_set.json --poa-input-data "$POA_INPUT" --output-dir "$TESTS" --depth 25
+ANON_SET_SIZE=100
+MERKLE_TREE_HEIGHT=25
 
-npx ts-node "$SCRIPTS"/input_prep_for_layer_one.ts --poa-input-data "$POA_INPUT" --write-layer-one-data-to "$TESTS"/layer_one/input.json
+MSG="GENERATING TEST INPUT FOR PROOF OF ASSETS PROTOCOL"
+execute npx ts-node "$TESTS"/generate_test_input.ts --num-sigs 2 --message "message to sign"
 
+MSG="GENERATING ANONYMITY SET of size $ANON_SET_SIZE"
+execute npx ts-node "$TESTS"/generate_anon_set.ts --num-addresses $ANON_SET_SIZE
+
+MSG="GENERATING MERKLE TREE FOR ANONYMITY SET (HEIGHT $MERKLE_TREE_HEIGHT), AND MERKLE PROOFS FOR OWNED ADDRESSES"
+execute npx ts-node "$SCRIPTS"/merkle_tree.ts  --anonymity-set "$TESTS"/anonymity_set.json --poa-input-data "$POA_INPUT" --output-dir "$TESTS" --height $MERKLE_TREE_HEIGHT
+
+MSG="PREPARING INPUT SIGNALS FILE FOR LAYER ONE CIRCUIT"
+execute npx ts-node "$SCRIPTS"/input_prep_for_layer_one.ts --poa-input-data "$POA_INPUT" --write-layer-one-data-to "$TESTS"/layer_one/input.json
+
+MSG="RUNNING PROVING SYSTEM FOR LAYER ONE CIRCUIT"
+printf "\n================ $MSG ================\n"
 "$TESTS"/layer_one/layer_one.sh
 
-python "$SCRIPTS"/sanitize_groth16_proof.py "$BUILD"/tests/layer_one
+MSG="CONVERTING LAYER ONE PROOF TO LAYER TWO INPUT SIGNALS"
+execute python "$SCRIPTS"/sanitize_groth16_proof.py "$BUILD"/tests/layer_one
+
+MSG="PREPARING INPUT SIGNALS FILE FOR LAYER TWO CIRCUIT"
 npx ts-node "$SCRIPTS"/input_prep_for_layer_two.ts --poa-input-data "$POA_INPUT" --merkle-root "$MERKLE_ROOT" --merkle-proofs "$MERKLE_PROOFS" --layer-one-proof-dir "$BUILD"/tests/layer_one/sanitized_proof.json --write-layer-two-data-to "$TESTS"/layer_two/input.json
 
+MSG="RUNNING PROVING SYSTEM FOR LAYER TWO CIRCUIT"
+printf "\n================ $MSG ================\n"
 "$TESTS"/layer_two/layer_two.sh
