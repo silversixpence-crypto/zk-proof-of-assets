2fcefb60b34085687435c4f4248029077f0bba70
Stent <stentonian@protonmail.com>
2024-02-29 13:25:49 +0000

    Rename some vars in full_workflow_parallel

--- tests/full_workflow_parallel.sh
+++ tests/full_workflow_parallel.sh
@@ -7,80 +7,91 @@ THIS_DIR="$(dirname "$THIS_FILE_PATH")"
 . "$THIS_DIR/../scripts/lib/cmd_executor.sh"
 
 # ///////////////////////////////////////////////////////
-# General env vars.
-
-NUM_SIGS=2
-ANON_SET_SIZE=10000
-MERKLE_TREE_HEIGHT=25
-PARALLELISM=1
+# General constants.
 
 SCRIPTS="$THIS_DIR"/../scripts
-BUILD_DIR="$THIS_DIR"/../build
+BUILD="$THIS_DIR"/../build
 TESTS="$THIS_DIR"/"$NUM_SIGS"_sigs
 LOGS="$TESTS"/logs
 POA_INPUT="$THIS_DIR"/input_data_for_"$NUM_SIGS"_accounts.json
 MERKLE_ROOT="$THIS_DIR"/merkle_root.json
 MERKLE_PROOFS="$THIS_DIR"/merkle_proofs.json
 
-printf "
-/////////////////////////////////////////////////////////
-Initiating test for the following data:
-
-- Number of accounts/signatures:   $NUM_SIGS
-- Anonymity set size:              $ANON_SET_SIZE
-- Merkle tree height:              $MERKLE_TREE_HEIGHT
-- Parallelism:                     $PARALLELISM
-
-/////////////////////////////////////////////////////////
-"
-
 # ///////////////////////////////////////////////////////
 # Create directories.
 
-if [[ ! -d "$BUILD_DIR" ]]; then
-	mkdir -p "$BUILD_DIR"
+if [[ ! -d "$BUILD" ]]; then
+	  mkdir -p "$BUILD"
 fi
 
 if [[ ! -d "$TESTS" ]]; then
-	mkdir -p "$TESTS"
+	  mkdir -p "$TESTS"
 fi
 
 if [[ ! -d "$LOGS" ]]; then
-	mkdir -p "$LOGS"
+	  mkdir -p "$LOGS"
 fi
 
 # ///////////////////////////////////////////////////////
-# Layer-specific env vars.
+# Layer-specific constants.
 
-L1_BUILD="$BUILD_DIR"/tests/layer_one
-L1_CIRCUIT_PATH="$TESTS"/layer_one.circom
-L1_PTAU_PATH="$THIS_DIR"/../powersOfTau28_hez_final_26.ptau
+L1_BUILD="$BUILD"/tests/layer_one
+L1_CIRCUIT="$TESTS"/layer_one.circom
+L1_PTAU="$THIS_DIR"/../powersOfTau28_hez_final_26.ptau
 L1_SIGNALS="$TESTS"/layer_one_input.json
 
-L2_BUILD="$BUILD_DIR"/tests/layer_two
-L2_CIRCUIT_PATH="$TESTS"/layer_two.circom
+L2_BUILD="$BUILD"/tests/layer_two
+L2_CIRCUIT="$TESTS"/layer_two.circom
 L2_PTAU_PATH="$THIS_DIR"/../powersOfTau28_hez_final_26.ptau
 L2_SIGNALS="$TESTS"/layer_two_input.json
 
-L3_BUILD="$BUILD_DIR"/tests/layer_three
+L3_BUILD="$BUILD"/tests/layer_three
 L3_CIRCUIT_PATH="$TESTS"/layer_three.circom
 L3_PTAU_PATH="$THIS_DIR"/../powersOfTau28_hez_final_26.ptau
 L3_SIGNALS="$TESTS"/layer_three_input.json
 
 # ///////////////////////////////////////////////////////
+# Variables.
+
+num_sigs=2
+anon_set_size=10000
+merkle_tree_height=25
+
+threshold=10
+parallelism=$((num_sigs / threshold))
+remainder=0
+
+if [[ $((parallelism * threshold)) < $num_sigs ]]; then
+    remainder=$((num_sigs - parallelism * threshold))
+    parallelism=$((parallelism + 1))
+fi
+
+printf "
+/////////////////////////////////////////////////////////
+Initiating test for the following data:
+
+- Number of accounts/signatures:   $num_sigs
+- Anonymity set size:              $anon_set_size
+- Merkle tree height:              $merkle_tree_height
+- Parallelism:                     $parallelism
+
+/////////////////////////////////////////////////////////
+"
+
+# ///////////////////////////////////////////////////////
 # Data generation
 
 MSG="GENERATING TEST CIRCUITS"
-execute npx ts-node "$THIS_DIR"/generate_test_circuits.ts --num-sigs $NUM_SIGS --tree-height $MERKLE_TREE_HEIGHT --parallelism $PARALLELISM
+execute npx ts-node "$THIS_DIR"/generate_test_circuits.ts --num-sigs $num_sigs --tree-height $merkle_tree_height --parallelism $parallelism
 
 MSG="GENERATING TEST INPUT FOR PROOF OF ASSETS PROTOCOL"
-execute npx ts-node "$THIS_DIR"/generate_test_input.ts --num-sigs $NUM_SIGS --message "message to sign"
+execute npx ts-node "$THIS_DIR"/generate_test_input.ts --num-sigs $num_sigs --message "message to sign"
 
 MSG="GENERATING ANONYMITY SET"
-execute npx ts-node "$THIS_DIR"/generate_anon_set.ts --num-addresses $ANON_SET_SIZE
+execute npx ts-node "$THIS_DIR"/generate_anon_set.ts --num-addresses $anon_set_size
 
 # Run in parallel to the next commands, 'cause it takes long
-(MSG="GENERATING MERKLE TREE FOR ANONYMITY SET, AND MERKLE PROOFS FOR OWNED ADDRESSES" execute npx ts-node "$SCRIPTS"/merkle_tree.ts --anonymity-set "$THIS_DIR"/anonymity_set.json --poa-input-data "$POA_INPUT" --output-dir "$THIS_DIR" --height $MERKLE_TREE_HEIGHT) &
+(MSG="GENERATING MERKLE TREE FOR ANONYMITY SET, AND MERKLE PROOFS FOR OWNED ADDRESSES" execute npx ts-node "$SCRIPTS"/merkle_tree.ts --anonymity-set "$THIS_DIR"/anonymity_set.json --poa-input-data "$POA_INPUT" --output-dir "$THIS_DIR" --height $merkle_tree_height) &
 
 # ///////////////////////////////////////////////////////
 # G16 setup for all layers, in parallel.
@@ -88,12 +99,12 @@ execute npx ts-node "$THIS_DIR"/generate_anon_set.ts --num-addresses $ANON_SET_S
 # TODO check number of sigs and only do the -b flag if there are more than 10M constraints
 (
 	printf "\n================ RUNNING G16 SETUP FOR LAYER 1 CIRCUIT (SEE $LOGS/layer_one_setup.log) ================\n" &&
-		"$SCRIPTS"/g16_setup.sh -b -B "$L1_BUILD" "$L1_CIRCUIT_PATH" "$L1_PTAU_PATH" >"$LOGS"/layer_one_setup.log 2>&1
+		"$SCRIPTS"/g16_setup.sh -b -B "$L1_BUILD" "$L1_CIRCUIT" "$L1_PTAU" >"$LOGS"/layer_one_setup.log 2>&1
 ) &
 
 (
 	printf "\n================ RUNNING G16 SETUP FOR LAYER 2 CIRCUIT (SEE $LOGS/layer_two_setup.log) ================\n" &&
-		"$SCRIPTS"/g16_setup.sh -b -B "$L2_BUILD" "$L2_CIRCUIT_PATH" "$L2_PTAU_PATH" >"$LOGS"/layer_two_setup.log 2>&1
+		"$SCRIPTS"/g16_setup.sh -b -B "$L2_BUILD" "$L2_CIRCUIT" "$L2_PTAU_PATH" >"$LOGS"/layer_two_setup.log 2>&1
 ) &
 
 (
@@ -109,7 +120,7 @@ wait
 MSG="PREPARING INPUT SIGNALS FILE FOR LAYER 1 CIRCUIT"
 execute npx ts-node "$SCRIPTS"/input_prep_for_layer_one.ts --poa-input-data "$POA_INPUT" --write-layer-one-data-to "$L1_SIGNALS"
 
-"$SCRIPTS"/g16_prove.sh -b -B "$L1_BUILD" "$L1_CIRCUIT_PATH" "$L1_SIGNALS"
+"$SCRIPTS"/g16_prove.sh -b -B "$L1_BUILD" "$L1_CIRCUIT" "$L1_SIGNALS"
 
 # ///////////////////////////////////////////////////////
 # Layer 2 prove.
@@ -123,7 +134,7 @@ execute npx ts-node "$SCRIPTS"/input_prep_for_layer_two.ts --poa-input-data "$PO
 MSG="RUNNING PROVING SYSTEM FOR LAYER TWO CIRCUIT"
 printf "\n================ $MSG ================\n"
 
-"$SCRIPTS"/g16_prove.sh -b -B "$L2_BUILD" "$L2_CIRCUIT_PATH" "$L2_SIGNALS"
+"$SCRIPTS"/g16_prove.sh -b -B "$L2_BUILD" "$L2_CIRCUIT" "$L2_SIGNALS"
 
 # ///////////////////////////////////////////////////////
 # Layer 3 prove.
